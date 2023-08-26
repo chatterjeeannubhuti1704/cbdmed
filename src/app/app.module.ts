95795e808a41a14412d1742e9420979004e20081
Annubhuti Chatterjee <89660859+chatterjeeannubhuti1704@users.noreply.github.com>
2023-08-26 20:54:16 +0530

    moduleupdates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import{HashLocationStrategy,LocationStrategy} from '@angular/common'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainbodyComponent } from './mainbody/mainbody.component';
@@ -23,7 +23,7 @@ import{PricingModule} from './pricing/pricing.module'
     BrowserModule,
     AppRoutingModule,
   ],
-  providers: [],
+  providers: [{provide:LocationStrategy, useClass:HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
